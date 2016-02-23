aff9db88bbf320245164a1920092cf8296821c3c
Ramses Sotelo <rsotelo2@stedwards.edu>
2016-02-22 22:26:55 -0600

    Added maven jar plugin and changed tabs to four spaces

--- src/main/java/com/pasotaku/cardtype/CardTypes.java
+++ src/main/java/com/pasotaku/cardtype/CardTypes.java
@@ -1,28 +1,28 @@
 package com.pasotaku.cardtype;
 
 public enum CardTypes {
-	NORMAL_CARD("normal", true),
-	SKIP("skip",false),
-	ATTACK("attack",false);
-	
-	private String cardType;
-	private boolean normalCard;
-	private CardTypes(String cardType, boolean normalCard) {
-		this.cardType = cardType;
-		this.normalCard = normalCard;
-	}
-	
-	public String getCardType() {
-		return cardType;
-	}
-	public void setCardType(String cardType) {
-		this.cardType = cardType;
-	}
-	public boolean isNormalCard() {
-		return normalCard;
-	}
-	public void setNormalCard(boolean normalCard) {
-		this.normalCard = normalCard;
-	}
-	
+    NORMAL_CARD("normal", true),
+    SKIP("skip",false),
+    ATTACK("attack",false);
+
+    private String cardType;
+    private boolean normalCard;
+    private CardTypes(String cardType, boolean normalCard) {
+        this.cardType = cardType;
+        this.normalCard = normalCard;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+    public boolean isNormalCard() {
+        return normalCard;
+    }
+    public void setNormalCard(boolean normalCard) {
+        this.normalCard = normalCard;
+    }
+
 };
--- src/main/java/com/pasotaku/cat/App.java
+++ src/main/java/com/pasotaku/cat/App.java
@@ -9,14 +9,14 @@ import org.apache.commons.io.FileUtils;
 public class App 
 {
     public static void main( String[] args ) {
-    	 File file = new File("jimmykun.txt");
-    	 try {
-			List lines = FileUtils.readLines(file, "UTF-8");
-			System.out.println(lines);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	System.out.println("hehehehehehehe");
+        File file = new File("jimmykun.txt");
+        try {
+            List lines = FileUtils.readLines(file, "UTF-8");
+            System.out.println(lines);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("hehehehehehehe");
     }
 }
